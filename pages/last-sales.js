9d88b586992c88a0d4318fd252973b5263c663e9
Tiago Lopes <36971635+tiagu2s@users.noreply.github.com>
2021-09-10 15:17:03 +0100

    SWR hook call

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,36 +1,64 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
   const [sales, setSales] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://next-js-course-fetching-default-rtdb.firebaseio.com/sales.json"
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://next-js-course-fetching-default-rtdb.firebaseio.com/sales.json"
-    )
-      .then((resp) => resp.json())
-      .then((data) => {
-        const transformedSalesToArray = [];
-        for (const key in data) {
-          transformedSalesToArray.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSalesToArray);
-        setIsLoading(false);
+    const transformedSalesToArray = [];
+    //   transform data from firebase to an array
+    for (const key in data) {
+      transformedSalesToArray.push({
+        id: key,
+        username: data[key].username,
+        volume: data[key].volume,
       });
-  }, []);
+    }
 
-  if (isLoading) {
-    <h1>Loading...</h1>;
+    setSales(transformedSalesToArray);
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       "https://next-js-course-fetching-default-rtdb.firebaseio.com/sales.json"
+  //     )
+  //       .then((resp) => resp.json())
+  //       .then((data) => {
+  //         const transformedSalesToArray = [];
+  //         for (const key in data) {
+  //           transformedSalesToArray.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSalesToArray);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  //   if (isLoading) {
+  //     <h1>Loading...</h1>;
+  //   }
+
+  //   if (!sales) {
+  //     <h1>No data yet...</h1>;
+  //   }
+
+  if (error) {
+    <h1>Failed Load data SWR</h1>;
   }
 
-  if (!sales) {
-    <h1>No data yet...</h1>;
+  if (!sales || !sales) {
+    <p>Loading..</p>;
   }
+
   return (
     <>
       <ul>
